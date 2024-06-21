b976fdf79bf5cc3821a0bf691853b8e663ca3a62
yskm <yangshiheclow@gamil.com>
2024-06-21 12:31:54 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        ppr: "incremental" // 值允许您对特定路由采用 PPR。
-    }
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
